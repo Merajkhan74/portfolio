746c106b5a4e321ba1ff5cb8b7bf097c45a45663
Meraj Khan <merajkhan05km@gmail.com>
2026-07-22 13:21:41 +0530

    Update Project component and add icons

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -4,6 +4,7 @@ import MerajImage from "../assets/MERAJ (2).jpg";
 import RazorPay from "../assets/Rezar-pay.jpeg";
 import MusicImage from "../assets/Music-page.jpeg";
 import SyncImage from "../assets/Sync_image.jpeg";
+import IconsPng from "../assets/icons.png";
 
 const Projects = () => {
   return (
@@ -39,24 +40,27 @@ const Projects = () => {
             <div className="flex gap-4">
               <Link
                 to={"https:razorpay-inspired-payment-platform-delta.vercel.app"}
-                className="px-4 py-2 bg-blue-500 hover:bg-blue-800 text-white text-black rounded font-semibold "
+                // className="px-4 py-2 bg-blue-500 hover:bg-blue-800 text-white text-black rounded font-semibold "
+                className="px-4 py-2 bg-white text-black rounded font-semibold "
               >
-                Live Demo
+                Live
+               <img src={IconsPng} alt="" className="w-6 h-6 inline-block ml-2" />
+
               </Link>
-              <Link
+              {/* <Link
                 to={
                   "https://github.com/Merajkhan74/Razorpay-Inspired-Payment-Platform-UI"
                 }
                 className="px-4 py-2 border border-white rounded hover:bg-amber-500"
               >
                 GitHub
-              </Link>
+              </Link> */}
             </div>
           </div>
         </div>
 
         {/* project 2  */}
-        <div className="flex flex-col lg:flex-row items-center min-w-60 max-w-[1080px] justify-between mx-auto gap-7 border border-white rounded-md p-2 mt-10 ">
+        <div className="flex flex-col lg:flex-row items-center min-w-80 max-w-[1080px] justify-between mx-auto gap-7 border border-white rounded-md p-2 mt-10 ">
           <div className="left space-y-6 h-[400px] max-w-[450px] overflow-y-scroll no-scrollbar">
             <img src={MusicImage} alt="" className="min-w-[400px]" />
           </div>
@@ -66,14 +70,14 @@ const Projects = () => {
               Music Streaming Website
             </h3>
 
-            <p className="text-gray-300 text-sm mb-4">
+            <p className="text-gray-300 text-sm mb-4 mr-4">
               A modern responsive music website interface designed to provide an
               engaging user experience for music discovery. The project focuses
               on clean UI design, responsive layouts, and structured content
               sections similar to real-world music streaming platforms.
             </p>
 
-            <ul className="text-sm text-gray-400 mb-4 space-y-1">
+            <ul className="text-sm text-gray-400 mb-4 space-y-3">
               <li>• Fully Responsive Music Website</li>
               <li>• Modern UI & Visual Hierarchy</li>
               <li>• Interactive Layout Sections</li>
@@ -82,38 +86,40 @@ const Projects = () => {
 
             <div className="flex gap-4">
               <Link
-                to={"https://music-website-umim.vercel.app/"}
+                to={"https://music-website-6z9d.vercel.app/"}
                 target="_blank"
-                className="px-4 py-2 bg-blue-500 hover:bg-blue-800 text-white text-black rounded font-semibold "
-              >
-                Live Demo
+              //   className="px-4 py-2 bg-blue-500 hover:bg-blue-800 text-white text-black rounded font-semibold "
+              className="px-4 py-2 bg-white text-black rounded font-semibold ">
+                Live
+                <img src={IconsPng} alt="" className="w-6 h-6 inline-block ml-2" />
               </Link>
 
-              <Link
+              {/* <Link
                 to={"https://github.com/Merajkhan74/Music-Website"}
                 className="px-4 py-2 border border-white rounded hover:bg-amber-500"
               >
                 GitHub
-              </Link>
+              </Link> */}
             </div>
           </div>
         </div>   
         {/* Project 03  */}
            <div className="flex flex-col lg:flex-row items-center min-w-60 max-w-[1080px] justify-between mx-auto gap-7 border border-white rounded-md p-2 mt-10 ">
           <div className="left space-y-6 h-[400px] max-w-[450px] overflow-y-scroll no-scrollbar">
-            <img src={SyncImage} alt="" className="min-w-[400px]" />
+            <img src={SyncImage} alt="" className="min-w-[400px] "  />
           </div>
 
           <div className="bg-white/10 backdrop-blur-lg rounded-xl p-6 hover:scale-102 transition shadow-lg h-[400px] min-w-[450px]">
             <h3 className="text-xl font-semibold mb-3 text-amber-400">
-              Music Streaming Website
+              StudySync
             </h3>
+           
 
             <p className="text-gray-300 text-sm mb-4">
               StudySync is a responsive and visually engaging learning platform UI built with React and Tailwind CSS. The project focuses on delivering a seamless and modern user experience with clean component architecture and structured layout design.
             </p>
 
-            <ul className="text-sm text-gray-400 mb-4 space-y-1">
+            <ul className="text-sm text-gray-400 mb-4 space-y-2">
               <li>•Built using React + Vite for fast development and optimized performance</li>
               <li>• Designed scalable and reusable UI components</li>
               <li>• Implemented responsive design using Tailwind CSS</li>
@@ -126,17 +132,18 @@ const Projects = () => {
               <Link
                 to={"https://study-sync-nu-eight.vercel.app/"}
                 target="_blank"
-                className="px-4 py-2 bg-blue-500 hover:bg-blue-800 text-white text-black rounded font-semibold "
+                className="px-4 py-2 bg-white text-black rounded font-semibold "
               >
-                Live Demo
+                Live
+                <img src={IconsPng} alt="" className="w-6 h-6 inline-block ml-2" />
               </Link>
 
-              <Link
+              {/* <Link
                 to={"https://github.com/Merajkhan74/StudySync"}
                 className="px-4 py-2 border border-white rounded hover:bg-amber-500"
               >
                 GitHub
-              </Link>
+              </Link> */}
             </div>
           </div>
         </div>
